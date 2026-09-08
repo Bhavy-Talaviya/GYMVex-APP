c81c80eeb1241484fcbafa0148a55d73be341d67
Bhavy-Talaviya <bhavy.talaviya.cg@gmail.com>
2026-09-08 22:39:12 +0530

    Configure EAS Android APK build and Render backend

--- GYMVex/eslint.config.js
+++ GYMVex/eslint.config.js
@@ -1,10 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-module.exports = defineConfig([
-  expoConfig,
+module.exports = [
+  ...(Array.isArray(expoConfig) ? expoConfig : [expoConfig]),
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', '.expo/*', 'backend/*'],
   },
-]);
+];
+
